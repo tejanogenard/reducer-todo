9b3c7b09caf97df2016aa3c16e897961940df349
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-03-16 13:01:03 -0700

    set inital state

---
+++ todo/src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { useState, useReducer } from 'react'
+import { formReducer, initalState } from '../reducers/formReducer'
+
+
+const Form = () => {
+
+const [formList, setFormList] = useState('')
+   
+
+
+
+}
+export default Form 
---
+++ todo/src/reducers/formReducer.js
@@ -0,0 +1,12 @@
+
+
+
+export const initalState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 1
+}
+
+export const simpleReducer = (state, action) => {
+
+}
