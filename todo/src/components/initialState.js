d5e40df33e36d607f255bf242a18c4f00aca7109
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-04-13 19:10:05 -0700

    create initial state

---
+++ todo/src/components/initialState.js
@@ -0,0 +1,36 @@
+import id from 'uuid/v4';
+
+const initalState = [
+    {
+        id: id(),
+        task: 'Learn React', 
+        completed: false
+    },
+    {
+        id: id(),
+        task: 'Learn Reducer', 
+        completed: false
+    },
+    {
+        id: id(),
+        task: 'Learn to learn', 
+        completed: false
+    },
+    {
+        id: id(),
+        task: 'Reduce re-use reducer', 
+        completed: false
+    },
+    {
+        id: id(),
+        task: 'task', 
+        completed: false
+    },
+    {
+        id: id(),
+        task: 'another task', 
+        completed: false
+    },
+]
+
+export default initalState
